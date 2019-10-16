8795897f02b081d12ca379b918680b180306e396
IWriteCuteBUG <1052299507@qq.com>
2019-10-16 14:47:25 +0800

    d:

--- guns-order/src/main/java/com/stylefeng/guns/rest/GunsOrderApplication.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/GunsOrderApplication.java
@@ -1,9 +1,11 @@
 package com.stylefeng.guns.rest;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.stylefeng.guns.rest"})
+@EnableDubboConfiguration
 public class GunsOrderApplication {
 
     public static void main(String[] args) {

ecb4defe691cd1ee10988623b52aca9712cf0e31
IWriteCuteBUG <1052299507@qq.com>
2019-10-18 00:18:31 +0800

    check接口

--- guns-api/src/main/java/com/stylefeng/guns/rest/vo/zyp/UserCheckBaseVo.java
+++ guns-api/src/main/java/com/stylefeng/guns/rest/vo/zyp/UserCheckBaseVo.java
@@ -19,7 +19,7 @@ public class UserCheckBaseVo implements Serializable {
 
     public static UserCheckBaseVo err() {
         UserCheckBaseVo userCheckBaseVo = new UserCheckBaseVo();
-        userCheckBaseVo.setStatus(2);
+        userCheckBaseVo.setStatus(1);
         userCheckBaseVo.setMsg("用户已注册");
         return userCheckBaseVo;
     }

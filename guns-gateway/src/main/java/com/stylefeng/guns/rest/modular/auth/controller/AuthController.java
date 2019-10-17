fd5dafc0df8b8aa7807dfeb6be1f47c5b4dc5965
IWriteCuteBUG <1052299507@qq.com>
2019-10-18 00:12:55 +0800

    优化登录

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/controller/AuthController.java
@@ -69,7 +69,7 @@ public class AuthController {
             AuthResponse body = new AuthResponse(token, randomKey);
             return AuthBasesVo.ok(body);
         } else {
-            throw new GunsException(1,"用户名或密码错误");
+            return AuthBasesVo.err();
         }
     }
 }
--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/model/AuthBasesVo.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/auth/model/AuthBasesVo.java
@@ -6,11 +6,19 @@ import org.springframework.http.ResponseEntity;
 public class AuthBasesVo {
     private int status;
     private Object data;
+    private String msg;
 
     public static AuthBasesVo ok(Object object) {
         AuthBasesVo authBasesVo = new AuthBasesVo();
         authBasesVo.setData(object);
         authBasesVo.setStatus(0);
+        return authBasesVo;
+    }
+
+    public static AuthBasesVo err() {
+        AuthBasesVo authBasesVo = new AuthBasesVo();
+        authBasesVo.setMsg("用户名或密码错误");
+        authBasesVo.setStatus(1);
         return authBasesVo;
     }
 
@@ -28,5 +36,13 @@ public class AuthBasesVo {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 }

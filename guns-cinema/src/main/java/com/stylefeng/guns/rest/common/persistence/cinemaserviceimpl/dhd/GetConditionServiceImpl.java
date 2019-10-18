019662de77a05e625f4804ef05ba16bac62ac695
IWriteCuteBUG <1052299507@qq.com>
2019-10-18 17:53:18 +0800

    修复1

---
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/cinemaserviceimpl/dhd/GetConditionServiceImpl.java
@@ -0,0 +1,76 @@
+package com.stylefeng.guns.rest.common.persistence.cinemaserviceimpl.dhd;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.stylefeng.guns.rest.cinemaservice.dhd.dhd.GetConditionService;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeAreaDictTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeBrandDictTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeHallDictTMapper;
+import com.stylefeng.guns.rest.common.persistence.vo.dhd.Area;
+import com.stylefeng.guns.rest.common.persistence.vo.dhd.Brand;
+import com.stylefeng.guns.rest.common.persistence.vo.dhd.Hall;
+import com.stylefeng.guns.rest.vo.dhd.AreaVo;
+import com.stylefeng.guns.rest.vo.dhd.BrandVo;
+import com.stylefeng.guns.rest.vo.dhd.HallTypeVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service(interfaceClass = GetConditionService.class)
+@Component
+public class GetConditionServiceImpl implements GetConditionService {
+    @Autowired
+    MtimeAreaDictTMapper areaDictTMapper;
+    @Override
+    public List<AreaVo> queryAreaList(int id) {
+        List<Area> areas = areaDictTMapper.selectAllArea();
+        List<AreaVo> areaVos = new ArrayList<>();
+        for (Area area : areas) {
+            AreaVo areaVo = new AreaVo();
+            areaVo.setAreaId(area.getUUID());
+            areaVo.setAreaName(area.getShow_name());
+            areaVo.setActive(false);
+            if (areaVo.getAreaId()==id){
+                areaVo.setActive(true);
+            }
+            areaVos.add(areaVo);
+        }
+        return areaVos;
+    }
+   @Autowired
+   MtimeBrandDictTMapper mtimeBrandDictTMapper;
+    @Override
+    public List<BrandVo> brandList(int id) {
+        List<Brand> brands = mtimeBrandDictTMapper.selectAllBrand();
+        ArrayList<BrandVo> brandVos = new ArrayList<>();
+        for (Brand brand : brands) {
+            BrandVo brandVo = new BrandVo();
+            brandVo.setBrandId(brand.getUUID());
+            brandVo.setBrandName(brand.getShow_name());
+            brandVo.setActive(false);
+            if(brandVo.getBrandId()==id){
+                brandVo.setActive(true);
+            }brandVos.add(brandVo);
+        }
+        return brandVos;
+    }
+     @Autowired
+     MtimeHallDictTMapper mtimeHallDictTMapper;
+    @Override
+    public List<HallTypeVo> hallList(int id) {
+      List<Hall>  halls=mtimeHallDictTMapper.selectAllHand();
+        ArrayList<HallTypeVo> hallTypeVos = new ArrayList<>();
+        for (Hall hall : halls) {
+            HallTypeVo hallTypeVo = new HallTypeVo();
+            hallTypeVo.setHalltypeId(hall.getUUID());
+            hallTypeVo.setHalltypeName(hall.getShow_name());
+            hallTypeVo.setActive(false);
+            if (hallTypeVo.getHalltypeId()==id){
+                hallTypeVo.setActive(true);
+            }
+            hallTypeVos.add(hallTypeVo);
+        }
+        return hallTypeVos;
+    }
+}
---
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/common/persistence/cinemaserviceimpl/sjb/CinemaServiceSJBImpl.java
@@ -0,0 +1,58 @@
+package com.stylefeng.guns.rest.common.persistence.cinemaserviceimpl.sjb;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.stylefeng.guns.rest.cinemabean.MtimeCinemaT;
+import com.stylefeng.guns.rest.cinemaservice.sjb.CinemaServiceSJB;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeCinemaTMapper;
+import com.stylefeng.guns.rest.common.persistence.dao.MtimeHallDictTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MtimeHallDictT;
+import com.stylefeng.guns.rest.vo.sjb.MtimeCinemaTVo;
+import com.stylefeng.guns.rest.vo.sjb.SeatObject;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+
+@Component
+@Service(interfaceClass = CinemaServiceSJB.class)
+public class CinemaServiceSJBImpl implements CinemaServiceSJB {
+
+    @Autowired
+    MtimeCinemaTMapper cinemaTMapper;
+
+    @Autowired
+    MtimeHallDictTMapper hallDictTMapper;
+
+    @Override
+    public MtimeCinemaTVo queryCinemaById(Integer cinemaId) {
+        MtimeCinemaT mtimeCinemaT = cinemaTMapper.selectById(cinemaId);
+        MtimeCinemaTVo mtimeCinemaTVo = new MtimeCinemaTVo();
+        BeanUtils.copyProperties(mtimeCinemaT, mtimeCinemaTVo);
+        return mtimeCinemaTVo;
+    }
+
+    @Override
+    public String getSeatObjectLocally(int hallId) throws IOException {
+        MtimeHallDictT mtimeHallDictT = hallDictTMapper.selectById(hallId);
+        String contextPath = this.getContex();
+        String path = contextPath + "\\guns-cinema\\src\\main\\resources\\" + mtimeHallDictT.getSeatAddress().replace("/", "\\");
+        System.out.println(path);
+        InputStream in = new FileInputStream(new File(path));
+        byte[] bytes = new byte[1];
+        int len = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((len = in.read(bytes)) != -1){
+            stringBuilder.append(new String(bytes));
+        }
+        in.close();
+        stringBuilder.append(new String(bytes));
+        return String.valueOf(stringBuilder);
+    }
+
+    private String getContex() {
+        File file = new File("");
+        return file.getAbsolutePath();
+    }
+}
